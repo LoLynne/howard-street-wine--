207771bcea59f1506b91e847e6625b32eb8f4cab
LoLynne <134532671+LoLynne@users.noreply.github.com>
2026-03-03 15:37:18 -0600

    Refactor App component to use React Router
    made change to App to help with button use

--- howard-street-wine--/src/App.jsx
+++ howard-street-wine--/src/App.jsx
@@ -1,34 +1,29 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { BrowserRouter, Routes, Route, Link } from 'react-router-dom'
 import './App.css'
+import About from './pages/About'
+import Events from './pages/Events'
+import Contact from './pages/Contact'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <BrowserRouter>
+      <nav>
+        <span className="nav-logo">Howard Street Wine Merchant</span>
+        <div className="nav-links">
+          <Link to="/">Shop</Link>
+          <Link to="/about">About</Link>
+          <Link to="/events">Events</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+      </nav>
+
+      <Routes>
+        <Route path="/" element={<h1>Shop Page Coming Soon</h1>} />
+        <Route path="/about" element={<About />} />
+        <Route path="/events" element={<Events />} />
+        <Route path="/contact" element={<Contact />} />
+      </Routes>
+    </BrowserRouter>
   )
 }
 
